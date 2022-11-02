846f72e5fb4f9dd9ea4cf19974d56c3547d194e3
Vinicius Leal Alves <59040683+lealalves@users.noreply.github.com>
2022-11-02 05:21:25 +0000

    using findById

--- myApp.js
+++ myApp.js
@@ -57,7 +57,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if(err) return console.log(err)
+    done(null, data)
+  })
 };
 
 const findEditThenSave = (personId, done) => {
