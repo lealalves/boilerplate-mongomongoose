206f02c1b09d1c26e19a0822b0682aeceb54a0eb
Vinicius Leal Alves <59040683+lealalves@users.noreply.github.com>
2022-11-02 03:33:12 +0000

    create person schema with mongoose

--- myApp.js
+++ myApp.js
@@ -1,12 +1,25 @@
 require('dotenv').config();
 
-let mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+  name: {
+    require: true,
+    type: String
+  },
+  age: {
+    require: true,
+    type: Number
+  },
+  favoriteFoods: {
+    type: Array
+  }
+})
 
-let Person;
+let Person = mongoose.model('Person', personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
